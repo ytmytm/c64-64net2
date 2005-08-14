495f4132697595027038d2350aa016fb90535efa
bitbreaker_voz <bitbreaker_voz>
2005-08-14 10:35:03 +0000

    removed acknowledge, now in lpt-comm.c

--- lptio/lptio_freebsd.c
+++ lptio/lptio_freebsd.c
@@ -55,11 +55,6 @@ void set_lpt_control(int status)
   ioctl(lpt_fd,PPISCTRL,&status);
 }
 
-void acknowledge() {
-  set_lpt_control(VALID_OUT);
-  set_lpt_control(0);
-}
-
 void write_data(unsigned char data) 
 {
   /* outb(portout,data); */
